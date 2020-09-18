40a55840a8c343d24dddc54aeab61be8c0726896
liyuhui666 <280470436@qq.com>
2020-09-18 21:28:24 +0800

    提交1

---
+++ src/main/java/com/atguigu/git/linghuchong.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+public class linghuchong {
+    public static void main(String[] args) {
+        System.out.println("hahahahhahah");
+        System.out.println("hahahahhahah");
+        System.out.println("hahahahhahah");
+        System.out.println("444444444444");
+        System.out.println("333333333333");
+        System.out.println("22222222222");
+    }
+}
